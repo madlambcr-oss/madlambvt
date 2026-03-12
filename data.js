e3266fadb4bf905c18b6f7ae233d19b2b4e247d6
madlambcr-oss <madlambcr@gmail.com>
2026-03-12 17:17:18 -0600

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ precio:"35,000",
 talla:"L",
 estado:"10/10",
 categoria:"hoodies",
-fotos:["hoodie1.jpg"],
+fotos:["Hoodie under armour real tree.jpg"],
 disponible:true
 },
 
@@ -16,37 +16,29 @@ precio:"50,000",
 talla:"L",
 estado:"8.5/10",
 categoria:"chaquetas",
-fotos:["detroit1.jpg","detroit2.jpg"],
+fotos:["Carharrt Detroit 100 años.jpg"],
 disponible:true
 }
 
-]
+];
 
 
 const bitacora = [
 
 {
 titulo:"The Waxed Canvas Trucker",
-
 fecha:"10 Marzo 2026",
-
 tiempo:"Lectura 3 min",
-
-imagen:"waxed.jpg",
-
+imagen:"The Waxed Canvas Trucker.jpg",
 texto:"La chaqueta de lona encerada es mucho más que una prenda..."
 },
 
 {
 titulo:"El Oro de Lona: La Detroit Jacket",
-
 fecha:"5 Marzo 2026",
-
 tiempo:"Lectura 2 min",
-
-imagen:"detroit.jpg",
-
+imagen:"El Oro de Lona La Detroit Jacket.jpg",
 texto:"Pocas piezas en la historia del vestuario masculino poseen la mística..."
 }
 
-]
+];
