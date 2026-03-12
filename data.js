4315459ca26274a3e108e92fecab81aa9b5c3d44
madlambcr-oss <madlambcr@gmail.com>
2026-03-12 17:26:28 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const prendas = [
 
 {
+id:1,
 nombre:"HOODIE UNDER ARMOUR 'REAL TREE'",
 precio:"35,000",
 talla:"L",
@@ -11,6 +12,7 @@ disponible:true
 },
 
 {
+id:2,
 nombre:"CARHARRT DETROIT '100 AÑOS'",
 precio:"50,000",
 talla:"L",
@@ -18,27 +20,49 @@ estado:"8.5/10",
 categoria:"chaquetas",
 fotos:["Carharrt Detroit 100 años.jpg"],
 disponible:true
-}
-
-];
-
-
-const bitacora = [
-
-{
-titulo:"The Waxed Canvas Trucker",
-fecha:"10 Marzo 2026",
-tiempo:"Lectura 3 min",
-imagen:"The Waxed Canvas Trucker.jpg",
-texto:"La chaqueta de lona encerada es mucho más que una prenda..."
 },
 
 {
-titulo:"El Oro de Lona: La Detroit Jacket",
-fecha:"5 Marzo 2026",
-tiempo:"Lectura 2 min",
-imagen:"El Oro de Lona La Detroit Jacket.jpg",
-texto:"Pocas piezas en la historia del vestuario masculino poseen la mística..."
+id:3,
+nombre:"WINDBREAKER PLAYBOY",
+precio:"25,000",
+talla:"M",
+estado:"9/10",
+categoria:"chaquetas",
+fotos:[
+"Windbreaker PlayBoy.jpg",
+"Windbreaker PlayBoy2.jpg",
+"Windbreaker PlayBoy3.jpg"
+],
+disponible:false
+},
+
+{
+id:4,
+nombre:"T-SHIRT GALLERY DEPT. 'TOKYO' EDITION",
+precio:"35,000",
+talla:"M",
+estado:"10/10",
+categoria:"camisetas",
+fotos:[
+"Gallery dept. tokyo edition.jpg",
+"Gallery dept. tokyo edition2.jpg"
+],
+disponible:true
+},
+
+{
+id:5,
+nombre:"ONEITA 'LOSER' TEE 'VINTAGE'",
+precio:"50,000",
+talla:"L",
+estado:"9/10",
+categoria:"camisetas",
+fotos:[
+"Oneita loser tee.jpg",
+"Oneita loser tee2.jpg"
+],
+disponible:true
 }
 
 ];
