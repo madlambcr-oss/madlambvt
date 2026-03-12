5759ec72fa04d37a766d077b7a0010bde08bed02
madlambcr-oss <madlambcr@gmail.com>
2026-03-12 17:04:57 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+const prendas = [
+
+{
+nombre:"HOODIE UNDER ARMOUR 'REAL TREE'",
+precio:"35,000",
+talla:"L",
+estado:"10/10",
+categoria:"hoodies",
+fotos:["hoodie1.jpg"],
+disponible:true
+},
+
+{
+nombre:"CARHARRT DETROIT '100 AÑOS'",
+precio:"50,000",
+talla:"L",
+estado:"8.5/10",
+categoria:"chaquetas",
+fotos:["detroit1.jpg","detroit2.jpg"],
+disponible:true
+}
+
+]
+
+
+const bitacora = [
+
+{
+titulo:"The Waxed Canvas Trucker",
+
+fecha:"10 Marzo 2026",
+
+tiempo:"Lectura 3 min",
+
+imagen:"waxed.jpg",
+
+texto:"La chaqueta de lona encerada es mucho más que una prenda..."
+},
+
+{
+titulo:"El Oro de Lona: La Detroit Jacket",
+
+fecha:"5 Marzo 2026",
+
+tiempo:"Lectura 2 min",
+
+imagen:"detroit.jpg",
+
+texto:"Pocas piezas en la historia del vestuario masculino poseen la mística..."
+}
+
+]
